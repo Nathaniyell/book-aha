c8d2370c076fe5c43535cd926dd06bad4e350240
Nathaniyell <essien.nathan@yahoo.com>
2023-10-14 20:19:31 +0100

    add data for second section

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -2,6 +2,30 @@ import React from "react";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
+
+    const data = [
+        {
+        title: "Organize your thoughts",
+        description: "Organise all the inspiration, ideas and lessons from books you’re reading.",
+        img: "",
+        smFlexDirection: "col",
+        lgFlexDirection: "row"
+    },
+        {
+        title: "Share your thoughts",
+        description: "Send your thoughts on books and articles to a community of readers like yourself",
+        img: "",
+        smFlexDirection: "col-reverse",
+        lgFlexDirection: "row-reverse"
+    },
+        {
+        title: "Discover new content",
+        description: "Find book reviews, new books, events and more exciting content curated for you.",
+        img: "",
+        smFlexDirection: "col",
+        lgFlexDirection: "row"
+    },
+    ]
   return (
     <>
       <div className="relative bg-heroSection bg-auto bg-no-repeat h-screen">
