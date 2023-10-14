5df178e042a185c56690ea02e0e8051796987dbc
Nathaniyell <essien.nathan@yahoo.com>
2023-10-14 19:46:30 +0100

    hero section done

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -7,9 +7,9 @@ const Home = () => {
       <div className="relative bg-heroSection bg-auto bg-no-repeat h-screen">
         <Navbar />
         <div className="relative">
-          <div className="w-[330px] bg-black md:w-1/5 h-[290px]bg-black text-white absolute md:right-52 top-20">
-            <h1 className="font-bold text-4xl relative left-32">
-              Share thoughts from books you love
+          <div className="w-[48%] bg-black md:w-1/4 text-white absolute top-24 md:top-20 right-52 p-2">
+            <h1 className="font-bold w-full relative text-[30px]">
+              <span className="relative -right-36">Share thoughts from</span> <br /><span className="relative -right-60">books you love</span>
             </h1>
           </div>
         </div>
