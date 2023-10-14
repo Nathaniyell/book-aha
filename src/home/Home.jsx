e80e491c677ab8c7fc6ba0c163b2a2dc07c2b67c
Nathaniyell <essien.nathan@yahoo.com>
2023-10-14 02:08:25 +0100

    update home component

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,8 +4,14 @@ import Navbar from "../components/Navbar";
 const Home = () => {
   return (
     <>
-      <div className="bg-heroSection bg-cover">
+      <div className="relative bg-heroSection bg-auto bg-no-repeat">
         <Navbar />
+        <div className="flex ">
+          <div className="w-[125px] h-[125px]  bg-black text-white">
+            {" "}
+            <h1>Share thoughts from books you love </h1>{" "}
+          </div>
+        </div>
       </div>
     </>
   );
