2979293a22e601a9e9a5ccf7aeed457f467a5565
Nathaniyell <essien.nathan@yahoo.com>
2023-10-17 17:29:12 +0100

    layout for signup page added

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import book1 from '../assets/book1.jpg'
+import book2 from '../assets/book2.jpg'
+import book3 from '../assets/book3.jpg'
+import book4 from '../assets/book4.jpg'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div className='flex bg-black text-white justify-between items-center h-screen w-full p-4 relative'>
+        <fieldset>
+       <form action="">
+        <label htmlFor="firstName">First Name:</label>
+        <input type="text" id='firstName' />
+        <label htmlFor="email">First Name:</label>
+        <input type="email" id='email' />
+        </form> 
+        </fieldset>
+        
+        </div>
   )
 }
 
