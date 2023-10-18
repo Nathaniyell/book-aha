dd8f8fa422ad21f5ae6f00aa0b0c0b27344dd91b
Nathaniyell <essien.nathan@yahoo.com>
2023-10-18 22:50:42 +0100

    update signup form

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -7,6 +7,8 @@ import {BsGithub} from 'react-icons/bs'
 import {FcGoogle} from "react-icons/fc"
 import {BsFillPersonFill} from "react-icons/bs"
 import {AiFillEye} from "react-icons/ai"
+import {MdEmail} from "react-icons/md"
+import {HiLockOpen} from "react-icons/hi"
 
 
 const About = () => {
@@ -56,9 +58,7 @@ const About = () => {
                   
                  <BsFillPersonFill className="text-gray-400 h-5 w-5 ml-3" />
                 </div>
-                <div className="absolute right-0 inset-y-0 flex items-center">
-                    <AiFillEye className="text-gray-400 h-5 w-5 mr-3" />
-                </div>
+                
               </div>
               <div class="relative mt-3">
                 
@@ -69,17 +69,7 @@ const About = () => {
                   placeholder="Email"
                 />
                 <div class="absolute left-0 inset-y-0 flex items-center">
-                  
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-7 w-7 ml-3 text-gray-400 p-1"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-                    
-                    <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z" />
-                    <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z" />
-                  </svg>
+                                    <MdEmail className="text-gray-400 h-5 w-5 ml-3" />
                 </div>
               </div>
               <div class="relative mt-3">
@@ -89,24 +79,19 @@ const About = () => {
                   id="username"
                   type="text"
                   placeholder="Password"
-                />{" "}
+                />
                 <div class="absolute left-0 inset-y-0 flex items-center">
            
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    class="h-7 w-7 ml-3 text-gray-400 p-1"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                  >
-             
-                    <path d="M10 2a5 5 0 00-5 5v2a2 2 0 00-2 2v5a2 2 0 002 2h10a2 2 0 002-2v-5a2 2 0 00-2-2H7V7a3 3 0 015.905-.75 1 1 0 001.937-.5A5.002 5.002 0 0010 2z" />{" "}
-                  </svg>{" "}
-                </div>{" "}
-              </div>{" "}
+                  <HiLockOpen className="text-gray-400 h-5 w-5 ml-3" />
+                </div>
+                <div className="absolute right-0 inset-y-0 flex items-center">
+                    <AiFillEye className="text-gray-400 h-5 w-5 mr-3 cursor-pointer" />
+                </div>
+              </div>
               <p class="mt-4 italic text-gray-500 font-light text-xs">
-                Password strength:{" "}
+                Password strength:
                 <span class="font-bold text-green-400">strong</span>
-              </p>{" "}
+              </p>
               <div class="mt-4 flex items-center text-gray-500">
          
                 <input
@@ -116,21 +101,21 @@ const About = () => {
                   class="mr-2"
                 />{" "}
                 <label class="text-sm" for="remember">
-                  I agree with the{" "}
+                  I agree with the
                   <a class="text-indigo-400 hover:text-indigo-500">
                     Privacy Policy
                   </a>
-                </label>{" "}
-              </div>{" "}
+                </label>
+              </div>
               <div class="flex items-center justify-center mt-8">
          
                 <button class="text-white py-2 px-4 uppercase rounded bg-indigo-500 hover:bg-indigo-600 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">
            
-                  Create Account{" "}
-                </button>{" "}
-              </div>{" "}
-            </form>{" "}
-          </div>{" "}
+                  Create Account
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
       </div>
     </div>
