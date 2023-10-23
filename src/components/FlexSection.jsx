c8e75a2fe4146268673869d6cf7d44c28c69fff6
Nathaniyell <essien.nathan@yahoo.com>
2023-10-23 01:32:59 +0100

    update the sections in the homepage

--- src/components/FlexSection.jsx
+++ src/components/FlexSection.jsx
@@ -17,7 +17,7 @@ const FlexSection = ({
 
   return (
     <section
-      className={`flex items-center ${flexDirection} justify-between w-8/12 mb-16 md:mb-12 mx-auto`}
+      className={`flex items-center ${flexDirection} justify-between w-10/12 mb-16 md:mb-12 mx-auto`}
     >
       <motion.div
      initial={{ x: -200, opacity: 0 }}
@@ -26,14 +26,14 @@ const FlexSection = ({
      viewport={{ once: false }} 
       >
         <h1 className="text-black text-3xl font-bold mb-4">{title}</h1>
-        <p className="w-9/12 text-lg mb-12 md:mb-0">{description}</p>
+        <p className="w-11/12 text-lg mb-12 md:mb-0">{description}</p>
       </motion.div>
       <motion.div
         initial={{ y: 100, opacity: 0 }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 1, delay: 0.5 }}
         viewport={{ once: false }}
-        className="bg-black grid items-center w-1/2 place-items-center md:w-2/5 md:mb-10"
+        className="bg-black grid items-center w-4/5 place-items-center md:w-3/5 md:mb-10"
       >
         <img className="w-full relative -top-5 left-5" src={img} alt={title} />
       </motion.div>
