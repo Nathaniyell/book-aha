d47d5e578c1e488d77d5f1e79c20497cb9a5c695
Nathaniyell <essien.nathan@yahoo.com>
2023-10-29 05:59:29 +0100

    update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom"; 
+import { Link } from "react-router-dom";
 import logo from "../assets/logo.png";
 import Button from "./Button";
-import USA from "../assets/us.png"
-import FR from "../assets/fr.png"
-import ESP from "../assets/esp.png"
+import USA from "../assets/us.png";
+import FR from "../assets/fr.png";
+import ESP from "../assets/esp.png";
 
 const Navbar = () => {
   const [colourChange, setColourChange] = useState(false);
@@ -32,7 +32,10 @@ const Navbar = () => {
         colourChange ? "bg-black" : "bg-inherit"
       }`}
     >
-     <Link to='/'> <img className="w-[40%] md:w-full text-white" src={logo} alt="Logo" /></Link>
+      <Link to="/">
+        {" "}
+        <img className="w-[40%] md:w-full text-white" src={logo} alt="Logo" />
+      </Link>
       <ul className="flex text-white items-center justify-around lg:w-1/4 -ml-32 md:-ml-0 w-[45%]">
         {["About", "SignUp"].map((item) => {
           return (
@@ -41,19 +44,33 @@ const Navbar = () => {
                 {item}
               </button>
             </Link>
-                      );
+          );
         })}
         <div>
           <span className="text-white bg-red-600">
             <select name="city" id="city" className="text-white">
-           <option value="1">{USA}EN</option>
-           <option value="2">{FR}FR</option>
-           <option value="3">{ESP}FR</option>
-           </select>
+              <option value="1">
+                <img src={USA} alt="EN-US" className="inline" />
+                EN
+              </option>
+              <option value="1">
+                <img src={FR} alt="FR" className="inline" />
+                FR
+              </option>
+              <option value="1">
+                <img src={ESP} alt="ESP" className="inline" />
+                ESP
+              </option>
+            </select>
           </span>
-               </div>
+        </div>
       </ul>
-      <Button classes="hidden sm:block" title={"Download App"} fontSize={"sm"} padding={6} />
+      <Button
+        classes="hidden sm:block"
+        title={"Download App"}
+        fontSize={"sm"}
+        padding={6}
+      />
     </div>
   );
 };
